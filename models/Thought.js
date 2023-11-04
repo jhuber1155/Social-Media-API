d3b8f8045f5d16c7799fce5fff006dcfcd2a3e62
jhuber1155 <141798215+jhuber1155@users.noreply.github.com>
2023-11-04 16:03:20 -0700

    Finished Thought Model and updated User with updated email validator

---
+++ models/Thought.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require('mongoose');
+const dayjs = require('dayjs');
+
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: new ObjectId ()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        max_length: 280
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dayjs(timestamp).format('YYYY-MM-DD'), 
+    },
+})
+
+const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        min_length: 1,
+        max_length: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dayjs(timestamp).format('YYYY-MM-DD'), 
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema]
+    
+    },
+    {
+      toJSON: { getters: true },
+      id: false,
+    }
+  );
+
+  thoughtSchema.virtual('reactionCount')/get(function (){
+    return this.reactions.length;
+});
+  
+  const Thought = model('Thought', thoughtSchema);
+  
+  module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,52 +1,43 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
-    {
-        userName: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true,
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(value);
         },
-        email: {
-            type: String,
-            unique: true,
-            required: true,
-            validate: {
-                validator: () => Promise.resolve(false),
-                message: 'Email validation failed'
-            }
-        },
-        thoughts: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Thought'
-            },
-        ],
-        friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            }
-        ]
-    });
+        message: 'Invalid email format'
+      }
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  }
+);
 
-    userSchema.virtual('friendCount')/get(function (){
-        return this.friends.length;
-    });
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
 
 const User = model('User', userSchema);
-const user = new User();
-
-user.email = 'test@test.co';
-
-let error;
-try {
-  await user.validate();
-} catch (err) {
-  error = err;
-}
-assert.ok(error);
-assert.equal(error.errors['email'].message, 'Email validation failed');
 
 module.exports = User;
